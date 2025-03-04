af6a00aa9cf2f84c9ac8194f1d59be25d0e1e75d
Ion Botnariuc <ionelbot23@gmail.com>
2025-03-04 20:15:32 +0200

    Initial commit.

---
+++ src/test/java/service_auto_ion/serviceion/ServiceionApplicationTests.java
@@ -0,0 +1,13 @@
+package service_auto_ion.serviceion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
